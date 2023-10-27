9f7101325179bcd6b4e352f9d1ba093353be1402
Sam <samarasamara.sf@gmail.com>
2023-10-27 14:37:15 +0530

    Added routing paths

--- src/App.js
+++ src/App.js
@@ -1,16 +1,61 @@
+import { useState } from "react";
 import "./App.css";
 import About from "./Components/About";
 import Navbar from "./Components/Navbar";
-// import TextForm from "./Components/TextForm";
+import TextForm from "./Components/TextForm";
+import Alert from "./Components/Alert";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
+  const [mode, setMode] = useState("light"); //Whether dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "grey";
+      showAlert("Dark Mode has been enabled", "success");
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light Mode has been enabled", "success");
+    }
+  };
+
   return (
     <div>
-      <Navbar title="TextUtils" aboutText="About Us" />
-      <div className="container my-3">
-        {/* <TextForm heading="Please enter the text" /> */}
-        <About />
-      </div>
+      <Router>
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+
+        <div className="container my-3">
+          <Routes>
+            <Route exact path="/about" element={<About />} />
+            <Route
+              exact
+              path="/"
+              element={
+                <TextForm
+                  showAlert={showAlert}
+                  heading="Please enter the text"
+                  mode={mode}
+                />
+              }
+            />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -6,15 +6,18 @@ export default function TextForm(props) {
   const handleUpClick = () => {
     let newText = text.toUpperCase();
     setText(newText);
+    props.showAlert("Converted to uppercase", "success");
   };
 
   const handleLoClick = () => {
     let newText = text.toLowerCase();
     setText(newText);
+    props.showAlert("Converted to lowercase", "success");
   };
 
   const clearText = () => {
     setText("");
+    props.showAlert("Cleared text", "success");
   };
 
   const handleOnChange = (event) => {
@@ -33,6 +36,10 @@ export default function TextForm(props) {
             rows="8"
             value={text}
             onChange={handleOnChange}
+            style={{
+              backgroundColor:
+                props.mode === "light" ? "lightgrey" : "lightgrey",
+            }}
           ></textarea>
         </div>
         <button className="btn btn-primary m-1" onClick={handleUpClick}>
@@ -53,7 +60,11 @@ export default function TextForm(props) {
         </p>
         <p>{0.008 * text.split(" ").length} minutes to read </p>
         <h5>Preview</h5>
-        <p>{text}</p>
+        <p>
+          {text.length > 0
+            ? text
+            : "Enter something in the textbox above to preview here"}
+        </p>
       </div>
     </>
   );
